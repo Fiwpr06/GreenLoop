1770c082a2a99070d02441aea769c24acd4b718b
Fiwpr06 <htfi597@gmail.com>
2025-12-09 01:04:16 +0700

    feat(server): add prisma schema and database configuration

---
+++ server/src/lib/prisma.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from "../generated/prisma";
+import "dotenv/config";
+
+const url = process.env.DATABASE_URL;
+if (!url) {
+  throw new Error("DATABASE_URL is not set in environment variables");
+}
+
+export const prisma = new PrismaClient({
+  log:
+    process.env.NODE_ENV === "development"
+      ? ["query", "error", "warn"]
+      : ["error"],
+});
